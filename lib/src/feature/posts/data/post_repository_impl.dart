ca9e13bb9318233d7f461276123e567c7b714156
LynnKhantKyaw <lynnkhant.ky@gmail.com>
2023-05-01 12:54:05 +0630

    removed fav repo

--- lib/src/feature/posts/data/post_repository_impl.dart
+++ lib/src/feature/posts/data/post_repository_impl.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_app_architecture/src/feature/posts/data/abstract_post_repository.dart';
 import 'package:flutter_app_architecture/src/feature/posts/domain/post_model.dart';
-import 'package:flutter_app_architecture/src/feature/posts/presentation/post_item_list_controller.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PostRepositoryImpl implements PostRepository {
@@ -19,12 +18,5 @@ class PostRepositoryImpl implements PostRepository {
   Future<void> toggleFavourite({required int id}) async {
     // api call for favourite or unfavourite
     ref.invalidate(getPostListProvider); //refresh post list after 200
-  }
-
-  @override
-  Future<List<PostModel>> getFavList() async {
-    await Future.delayed(const Duration(seconds: 1));
-    final favlist = ref.watch(postItemListControllerProvider.notifier).favList;
-    return favlist;
   }
 }
--- lib/src/feature/posts/presentation/fav_list_page.dart
+++ lib/src/feature/posts/presentation/fav_list_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_architecture/src/feature/posts/data/abstract_post_repository.dart';
 import 'package:flutter_app_architecture/src/feature/posts/presentation/post_item_card.dart';
+import 'package:flutter_app_architecture/src/feature/posts/presentation/post_item_list_controller.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FavListPage extends StatelessWidget {
@@ -18,31 +18,23 @@ class FavListPage extends StatelessWidget {
           children: [
             Expanded(
               child: Consumer(builder: (context, ref, _) {
-                return ref.watch(favListProvider).when(
-                      data: (data) {
-                        return ListView.separated(
-                          separatorBuilder: (_, __) => const SizedBox(
-                            height: 10,
-                          ),
-                          itemBuilder: (_, index) {
-                            final item = data[index];
-                            return PostItemCard(
-                              isFavIcon: false,
-                              name: item.name,
-                              favourite: item.favourite,
-                              toggleFavourite: () {},
-                            );
-                          },
-                          itemCount: data.length,
-                        );
-                      },
-                      error: (e, st) => Center(
-                        child: Text('$e'),
-                      ),
-                      loading: () => const Center(
-                        child: CircularProgressIndicator(),
-                      ),
+                final favList =
+                    ref.watch(postItemListControllerProvider.notifier).favList;
+                return ListView.separated(
+                  separatorBuilder: (_, __) => const SizedBox(
+                    height: 10,
+                  ),
+                  itemBuilder: (_, index) {
+                    final item = favList[index];
+                    return PostItemCard(
+                      isFavIcon: false,
+                      name: item.name,
+                      favourite: item.favourite,
+                      toggleFavourite: () {},
                     );
+                  },
+                  itemCount: favList.length,
+                );
               }),
             ),
           ],
